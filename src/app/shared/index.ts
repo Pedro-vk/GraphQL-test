701551a127a775e4d619f084e08601f07d64cb66
Pedro Gutiérrez <pedrogp9_6_93@hotmail.com>
2016-10-17 18:22:26 +0200

    Ordered the tags size in the sidebar.

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -13,8 +13,12 @@ export const interfaces = Interfaces;
 import { KeysPipe } from './keys.pipe';
 export { KeysPipe };
 
+import { OrderByOnSidebarPipe } from './order-by-on-sidebar.pipe';
+export { OrderByOnSidebarPipe };
+
 export const Pipes = [
   KeysPipe,
+  OrderByOnSidebarPipe,
 ];
 
 export { queries } from './queries';
---
+++ src/app/shared/order-by-on-sidebar.pipe.ts
@@ -0,0 +1,25 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({name: 'orderByOnSidebar'})
+export class OrderByOnSidebarPipe implements PipeTransform {
+  transform(value: any[], filter: string) {
+    if (!value) {
+      return [];
+    }
+    return value
+      .sort((a, b) => checkAttr(b) < checkAttr(a) ? 1 : -1);
+
+    function checkAttr(_: any): string | number {
+      return isNaN(+_[filter]) ? replaceSize(_[filter]) : +_[filter];
+    }
+    function replaceSize(key: string): string {
+      if (key.indexOf('size') === -1) {
+        return key;
+      }
+      return key
+        .replace('-s', '-2')
+        .replace('-m', '-3')
+        .replace('-l', '-4');
+    }
+  }
+}
