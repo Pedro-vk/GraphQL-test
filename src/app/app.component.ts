b9f6a2a3efbf10bf202efb8d2b72c9e4dce7dd24
Pedro Gutiérrez <pedrogp9_6_93@hotmail.com>
2016-11-07 17:18:10 +0100

    Cleaned the AppComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
-import { ApolloQueryResult } from 'apollo-client';
-import { Angular2Apollo, ApolloQueryObservable } from 'angular2-apollo';
-import { ClusterNode, Status, StatusStatus, Tag } from './shared/interfaces';
-import { queries, AttributeCounter } from './shared';
-import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'pgp-app',
