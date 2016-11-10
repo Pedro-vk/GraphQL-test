34c19695c1e49b093e04e0a831bf94abb83282df
Pedro Gutiérrez <pedrogp9_6_93@hotmail.com>
2016-11-10 01:56:00 +0100

    Added some time for show the loading.

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,28 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app';
 import { hotBootstrap } from './hmr';
 
-
-import { } from './app';
+const minLoadingDelay: number = 2000;
+const loadingHideTransition: number = 210;
 
 if (process.env.ENV === 'production') {
   enableProdMode();
 }
+
 function bootstrap(): Promise<NgModuleRef<any>> {
   return platformBrowserDynamic().bootstrapModule(AppModule);
 }
+function afterLoading(onLoaded: () => void) {
+  setTimeout(() => {
+    let loadingElement = document.querySelector('.loading');
+    if (loadingElement) {
+      loadingElement.classList.add('loading--hidden');
+    }
+    setTimeout(onLoaded, loadingHideTransition);
+  }, minLoadingDelay - Date.now() + (<any>window).loadingStart);
+};
 
 if ((<any>module).hot) {
-  hotBootstrap(module, bootstrap);
+  afterLoading(() => hotBootstrap(module, bootstrap));
 } else {
-  bootstrap();
+  afterLoading(() => bootstrap());
 }
